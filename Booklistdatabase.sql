2bc4b3ca313bc902ca5a068aff2313f4c7d5952f
Sr-Nacho <126293026+Sr-Nacho@users.noreply.github.com>
2023-03-27 12:00:04 -0600

    Create Booklistdatabase.sql

---
+++ Booklistdatabase.sql
@@ -0,0 +1,4 @@
+CREATE TABLE booklist (id INTEGER PRIMARY KEY, name TEXT, rating INTEGER);
+INSERT INTO booklist VALUES (1, "One Piece", 5);
+INSERT INTO booklist VALUES (2, "Harry Potter", 4.85);
+INSERT INTO booklist VALUES (3, "Hunger Games", 4.8);
